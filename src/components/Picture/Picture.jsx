717fdc0fc3135278dc1f42da5178b01ee18a2d9d
tramakarov <e2042912@gmail.com>
2020-04-21 22:04:21 +0500

    Add progressive image to QuestCard and Piccture

--- src/components/Picture/Picture.jsx
+++ src/components/Picture/Picture.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import ProgressiveImage from 'react-progressive-image'
 
 function Picture (props) {
   return (
-      <img width = "1150" height = "300" src={props.quest.imageUrl} className="img-fluid" alt="Responsive image"/>
+      <ProgressiveImage src={props.quest.imageUrl} placeholder="tiny-image.jpg">
+          {src => <img width = "1150" height = "300" src={src} className="img-fluid" alt="Responsive image" />}
+      </ProgressiveImage>
       )
 }
 
