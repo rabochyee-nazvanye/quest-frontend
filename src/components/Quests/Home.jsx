b4f4678bee8ac4f5dc2d7ec29aee26b79bcfec1a
Anna <anna.ilina.21.2000@gmail.com>
2020-04-22 01:03:37 +0500

    try to create account page

--- src/components/Quests/Home.jsx
+++ src/components/Quests/Home.jsx
@@ -3,7 +3,9 @@ import QuestCard from '../QuestCard/QuestCard'
 import { Spin } from 'antd'
 import { BASE_URL } from '../../settings'
 import { Row, Col } from 'antd'
-import {Link} from "react-router-dom";
+import {Link, Redirect} from "react-router-dom";
+import { Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
 
 class Home extends Component {
   constructor (props) {
@@ -34,24 +36,33 @@ class Home extends Component {
     } else {
       return (
         <React.Fragment>
+          <div>
+            <Avatar size={64} icon={<UserOutlined />} />
+          </div>
+            <div className={"content-container"}>
           {this.mapQuestsToTemplate()}
+            </div>
         </React.Fragment>
       )
     }
   }
 
   render () {
-    return (
-      <React.Fragment>
-        <p>
-          &#160; <p>&#160;</p>
-        <Row type="flex">
-          {this.getRepresentationByState()}
-        </Row>
-        </p>
-      </React.Fragment>
-    )
-  }
+    const redirectToAuth = (this.state.status === 401) ? (
+        <Redirect from={this.props.url} to={"/auth/" + encodeURIComponent(this.props.url)} />
+    ) : (<React.Fragment/>)
+      return (
+        <React.Fragment>
+          <p>
+            &#160; <p>&#160;</p>
+            <Row type="flex">
+              {this.getRepresentationByState()}
+            </Row>
+          </p>
+          {redirectToAuth}
+        </React.Fragment>)
+      }
+
 }
 
 export default Home
