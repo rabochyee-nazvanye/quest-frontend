180752cc12970be6c91c5f1c61dc748c757da627
Anna <anna.ilina.21.2000@gmail.com>
2020-04-22 01:02:54 +0500

    edit timeline viewing

--- src/components/QuestPage/QuestTimeline.jsx
+++ src/components/QuestPage/QuestTimeline.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Button, Col, Row, Steps} from 'antd'
+import {Button, Col, Row, Steps, Popover} from 'antd'
 import "./QuestTimeline.css"
 import { Loading3QuartersOutlined, CheckCircleOutlined, LoadingOutlined } from '@ant-design/icons'
 import { getToken } from '../../redux/Actions/Api.js';
@@ -7,57 +7,28 @@ import { useHistory } from 'react-router-dom'
 let dateTimeNow = new Date();
 
 
-const { Step } = Steps
-
-function addNull2Mins(props) {
-    return new props.getMinutes()<10 ? '0' + props.getMinutes().toString() : props.getMinutes().toString();
-}
-function addNull2Hours(props) {
-    return new props.getHours()<10 ? '0' + props.getHours().toString() : props.getHours().toString();
-}
-function addNull2Days(props) {
-    return new props.getDay()<10 ? '0' + props.getDay().toString() : props.getDay().toString();
-}
-function addNull2Months(props) {
-    return new props.getMonth()+1<10 ? '0' + (props.getMonth()+1).toString() : (props.getMonth()+1).toString();
+const { Step } = Steps;
+function RegDescription(props) {
+    return <div className={"description"}>{props}</div>
 }
 
 function QuestTimeline (props) {
-    const history = useHistory()
-
+    const history = useHistory();
     const remainingTime = new Date(props.quest.endDate).getHours()*60 + new Date(props.quest.endDate).getMinutes() -
         ((dateTimeNow.getUTCHours()+5)*60 + dateTimeNow.getMinutes());
     const remainingHours = parseInt(remainingTime / 60);
     const remainingMinutes = remainingTime % 60;
-
-    const regMin = new Date(props.quest.registrationDeadline).getMinutes()<10
-        ? '0' + new Date(props.quest.registrationDeadline).getMinutes().toString()
-        : new Date(props.quest.registrationDeadline).getMinutes().toString();
-
-    const startMin = new Date(props.quest.startDate).getMinutes()<10
-        ? '0' + new Date(props.quest.startDate).getMinutes().toString()
-        : new Date(props.quest.startDate).getMinutes().toString();
-
-    const endMin = new Date(props.quest.endDate).getMinutes()<10
-        ? '0' + new Date(props.quest.endDate).getMinutes().toString()
-        : new Date(props.quest.endDate).getMinutes().toString();
+    var commonOptions = {
+        year: 'numeric', month: 'numeric', day: 'numeric',
+        hour: 'numeric', minute: 'numeric',
+        hour12: false,
+        timeZone: 'America/Los_Angeles'
+    };
 
     const remainingTimeText = 'Осталось ' + remainingHours.toString() + ' ч ' + remainingMinutes.toString() + ' мин';
-    const regDeadline = 'до ' + new Date(props.quest.registrationDeadline).getDay().toString() + '/' +
-        (new Date(props.quest.registrationDeadline).getMonth()+1).toString() + '/' +
-        new Date(props.quest.registrationDeadline).getFullYear().toString() + '/ ' +
-        new Date(props.quest.registrationDeadline).getHours().toString() + ':' + regMin;
-
-    const startTime = new Date(props.quest.startDate).getDay().toString() + '/' +
-        (new Date(props.quest.startDate).getMonth()+1).toString() + '/' +
-        new Date(props.quest.startDate).getFullYear().toString() + '/ ' +
-        new Date(props.quest.startDate).getHours().toString() + ':' + startMin;
-
-    const endTime = new Date(props.quest.endDate).getDay().toString() + '/' +
-        (new Date(props.quest.endDate).getMonth()+1).toString() + '/' +
-        new Date(props.quest.endDate).getFullYear().toString() + '/ ' +
-        new Date(props.quest.endDate).getHours().toString() + ':' + endMin;
-
+    const regDeadline = 'до ' + new Intl.DateTimeFormat('default', commonOptions).format(new Date(props.quest.registrationDeadline));
+    const startTime = new Intl.DateTimeFormat('default', commonOptions).format(new Date(props.quest.startDate)).toString();
+    const endTime = new Intl.DateTimeFormat('default', commonOptions).format(new Date(props.quest.endDate)).toString();
     if (props.quest.status === "scheduled") {
         return (
             <React.Fragment>
@@ -84,7 +55,7 @@ function QuestTimeline (props) {
                     </Col>
                     <Col span={15} offset={1}>
                         <Steps current={0}>
-                            <Step title="Регистрация" subTitle="" description={regDeadline}/>
+                            <Step title="Регистрация" subTitle="" description={RegDescription(regDeadline)}/>
                             <Step title="Старт" subTitle="" description={startTime}/>
                             <Step title="Завершение" description={endTime}/>
                         </Steps>
@@ -102,7 +73,7 @@ function QuestTimeline (props) {
                     </Col>
                     <Col span={15} offset={1}>
                         <Steps current={1}>
-                            <Step title="Регистрация" subTitle="" description={regDeadline}/>
+                            <Step title="Регистрация" subTitle="" description={RegDescription(regDeadline)}/>
                             <Step title="Старт" subTitle={remainingTimeText} description={startTime}/>
                             <Step title="Заевршение" description={endTime}/>
                         </Steps>
@@ -111,7 +82,7 @@ function QuestTimeline (props) {
             </React.Fragment>
         )
     }
-    else {
+    else if (props.quest.status === "finished"){
         return (
             <React.Fragment>
                 <Row type="flex">
@@ -120,7 +91,7 @@ function QuestTimeline (props) {
                     </Col>
                     <Col span={15} offset={3}>
                         <Steps current={2}>
-                            <Step title="Регистрация" subTitle="" description={regDeadline}/>
+                            <Step title="Регистрация" subTitle="" description={RegDescription(regDeadline)}/>
                             <Step title="Старт" subTitle="" description={startTime}/>
                             <Step title="Завершён" description={endTime}/>
                         </Steps>
