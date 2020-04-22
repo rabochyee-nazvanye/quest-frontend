94ead95e6e8e66a07880004ea037df8941e4917c
toplenboren <toplenboren@yandex.ru>
2020-04-22 23:02:39 +0500

    Sentry and build hotfix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Footer from './components/shared/Footer/Footer'
 import About from './components/About/About'
 import GuestsList from './components/Account/AccountTemplate/GuestsList'
 import QuestPage from './components/QuestPage/QuestPage'
-import InvitePage from "./components/InvitePage/InvitePage";
+import InvitePage from './components/InvitePage/InvitePage'
 import './add-space.css'
 
 import './App.css'
@@ -22,7 +22,7 @@ import Auth from './components/Auth/Auth'
 import Account from './components/Account/Account'
 import { login } from './redux/Actions/Api'
 import NoMatch from './components/NoMatch/NoMatch'
-import QuestPlaymode from "./components/QuestPlaymode/QuestPlaymode";
+import QuestPlaymode from './components/QuestPlaymode/QuestPlaymode'
 
 function App () {
   useEffect(() => {
@@ -35,17 +35,17 @@ function App () {
         <div className={'container'}>
           <Header/>
           <div className={'add-space'}>
-          <Switch>
-            <Route path="/invites/:id" exact component={InvitePage}/>
-            <Route path="/quests/:id" exact component={QuestPage}/>
-            <Route exact path={'/quests/:id/play'} component={QuestPlaymode}/>
-            <Route exact path={'/'} component={Account}/>
-            <Route exact path={'/about'} component={About}/>
-            <Route exact path={'/auth'} component={Auth}/>
-            <Route exact path={'/auth/:redirectTo'} component={Auth} />
-            <Route exact path={'/account'} component={Account} />
-            <Route component={NoMatch} />
-          </Switch>
+            <Switch>
+              <Route path="/invites/:id" exact component={InvitePage}/>
+              <Route path="/quests/:id" exact component={QuestPage}/>
+              <Route exact path={'/quests/:id/play'} component={QuestPlaymode}/>
+              <Route exact path={'/'} component={Account}/>
+              <Route exact path={'/about'} component={About}/>
+              <Route exact path={'/auth'} component={Auth}/>
+              <Route exact path={'/auth/:redirectTo'} component={Auth} />
+              <Route exact path={'/account'} component={Account} />
+              <Route component={NoMatch} />
+            </Switch>
           </div>
         </div>
         <Footer/>
