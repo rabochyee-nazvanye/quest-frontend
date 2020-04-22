c83b0784c304bc36fb819066367f7e846b9db4c0
tramakarov <e2042912@gmail.com>
2020-04-22 17:21:56 +0500

    Update BASE_URL

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://api.questspace.live'
+export const BASE_URL = 'https://api.questspace.live'
 export const BACKEND_AUTH_PATH = '/session'
 export const BACKEND_AUTH_REGISTER_PATH = '/users'
 export const BACKEND_AUTH_FETCH_PATH = '/session'
