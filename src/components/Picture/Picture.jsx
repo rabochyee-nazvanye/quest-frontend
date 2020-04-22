8efee5989b3558f9fc626a0d3650bc6ac084ba75
tramakarov <e2042912@gmail.com>
2020-04-22 18:23:20 +0500

    Edit low image in Picture component

--- src/components/Picture/Picture.jsx
+++ src/components/Picture/Picture.jsx
@@ -3,7 +3,7 @@ import ProgressiveImage from 'react-progressive-image'
 
 function Picture (props) {
   return (
-      <ProgressiveImage src={props.quest.imageUrl} placeholder="tiny-image.jpg">
+      <ProgressiveImage src={props.quest.imageUrl} placeholder="ww-quest-low.jpg">
           {src => <img width = "1150" height = "300" src={src} className="img-fluid" alt="Responsive image" />}
       </ProgressiveImage>
       )
