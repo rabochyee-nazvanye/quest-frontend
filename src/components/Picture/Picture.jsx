cbb931cbbdc9366113c7afa1914733126c472702
tramakarov <e2042912@gmail.com>
2020-04-22 18:28:07 +0500

    Update Picture alt

--- src/components/Picture/Picture.jsx
+++ src/components/Picture/Picture.jsx
@@ -4,7 +4,7 @@ import ProgressiveImage from 'react-progressive-image'
 function Picture (props) {
   return (
       <ProgressiveImage src={props.quest.imageUrl} placeholder="/img/ww-quest-low.jpg">
-          {src => <img src={src} className="img-fluid" alt="Responsive image" />}
+          {src => <img src={src} className="img-fluid" alt="Картинка квеста" />}
       </ProgressiveImage>
       )
 }
