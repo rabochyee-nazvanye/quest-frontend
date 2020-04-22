eebaf592dacb14061b95a9d2a2f136199c7b0ffb
Anna <anna.ilina.21.2000@gmail.com>
2020-04-22 22:07:29 +0500

    fix timeline

--- src/components/QuestPage/QuestTimelineNoTeam.jsx
+++ src/components/QuestPage/QuestTimelineNoTeam.jsx
@@ -74,47 +74,45 @@ function QuestTimelineNoTeam (props) {
             </React.Fragment>
         )
     }
-    else if (props.quest.status === "inprogress"){
-        return (
-            <React.Fragment>
-                <div className={'left-box'}>
-                    <h6><EnvironmentTwoTone />
-                        <a href="https://ru.wikipedia.org/wiki/%D0%95%D0%BA%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%BD%D0%B1%D1%83%D1%80%D0%B3"
-                           className="btn btn-primary stretched-link">Екатеринбург, Россия</a></h6>
-                    <div className={'status-layout'} style={{"color": "#52c41a"} }><PlayCircleFilled /> &#160;Сейчас</div>
-                        &nbsp;
-                </div>
-                <div className={"vertical-line "}> </div>
-                <div className={'right-box'}>
-                    <h6>Организатор:  <Avatar src={props.quest.author.avatarUrl} size={'small'} /> {props.quest.author.name}</h6>
-                    <Steps current={1}>
-                        <Step title="Регистрация" subTitle="" description={RegDescription(regDeadline)}/>
-                        <Step title="Старт" subTitle={remainingTimeText} description={startTime}/>
-                        <Step title="Завершение" description={endTime}/>
-                    </Steps>
-                </div>
-            </React.Fragment>
-        )
-    }
     else if (props.quest.status === "registrationover")
     {
         return (
             <React.Fragment>
-                <div className={'left-box'}>
-                    <h6><EnvironmentTwoTone />
-                        <a href="https://ru.wikipedia.org/wiki/%D0%95%D0%BA%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%BD%D0%B1%D1%83%D1%80%D0%B3"
-                           className="btn btn-primary stretched-link">Екатеринбург, Россия</a></h6>
-                    <div className={'status-layout'} style={{"color": "#8c8c8c"}}> <CheckCircleOutlined />&#160;Регистрация окончена</div>
+                <p> </p>
+                <div className={'status-layout'} style={{"color": "#8c8c8c"} }><CheckCircleOutlined /> Регистрация окончена</div>
+                <div className={'button-layout'}>
                     &nbsp;
                 </div>
                 <div className={"vertical-line "}> </div>
                 <div className={'right-box'}>
-                    <h6>Организатор:  <Avatar src={props.quest.author.avatarUrl} size={'small'} /> {props.quest.author.name}</h6>
-                    <Steps current={1}>
-                        <Step title="Регистрация" subTitle="" description={RegDescription(regDeadline)}/>
-                        <Step title="Старт" subTitle="" description={startTime}/>
-                        <Step title="Завершение" description={endTime}/>
-                    </Steps>
+                    <div className={'timeline-layout'}>
+                        <Steps current={1}>
+                            <Step title="Регистрация" subTitle="" description={RegDescription(regDeadline)}/>
+                            <Step title="Старт" subTitle="" description={startTime}/>
+                            <Step title="Завершение" description={endTime}/>
+                        </Steps>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
+    else if (props.quest.status === "inprogress"){
+        return (
+            <React.Fragment>
+                <p> </p>
+                <div className={'status-layout'} style={{"color": "#52c41a"} }><PlayCircleFilled /> &#160;Сейчас</div>
+                <div className={'button-layout'}>
+                    &nbsp;
+                </div>
+                <div className={"vertical-line "}> </div>
+                <div className={'right-box'}>
+                    <div className={'timeline-layout'}>
+                        <Steps current={1}>
+                            <Step title="Регистрация" subTitle="" description={RegDescription(regDeadline)}/>
+                            <Step title="Старт" subTitle={remainingTimeText} description={startTime}/>
+                            <Step title="Завершение" description={endTime}/>
+                        </Steps>
+                    </div>
                 </div>
             </React.Fragment>
         )
@@ -122,20 +120,18 @@ function QuestTimelineNoTeam (props) {
     else {
         return (
             <React.Fragment>
-                <div className={'left-box'}>
-                    <h6><EnvironmentTwoTone />
-                        <a href="https://ru.wikipedia.org/wiki/%D0%95%D0%BA%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%BD%D0%B1%D1%83%D1%80%D0%B3"
-                           className="btn btn-primary stretched-link">Екатеринбург, Россия</a></h6>
-                    <div className={'status-layout'} style={{"color": "#8c8c8c"}}> <CheckCircleOutlined /> &#160;Завершён</div>
-                </div>
+                <p> </p>
+                <div className={'status-layout'} style={{"color": "#8c8c8c"} }><CheckCircleOutlined /> &#160;Завершён</div>
+                <div className={'button-layout'}>&nbsp; </div>
                 <div className={"vertical-line "}> </div>
                 <div className={'right-box'}>
-                    <h6>Организатор:  <Avatar src={props.quest.author.avatarUrl} size={'small'} /> {props.quest.author.name}</h6>
-                    <Steps current={2}>
-                        <Step title="Регистрация" subTitle="" description={RegDescription(regDeadline)}/>
-                        <Step title="Старт" subTitle="" description={startTime}/>
-                        <Step title="Завершён" description={endTime}/>
-                    </Steps>
+                    <div className={'timeline-layout'}>
+                        <Steps current={2}>
+                            <Step title="Регистрация" subTitle="" description={RegDescription(regDeadline)}/>
+                            <Step title="Старт" subTitle="" description={startTime}/>
+                            <Step title="Завершён" description={endTime}/>
+                        </Steps>
+                    </div>
                 </div>
             </React.Fragment>
         )
