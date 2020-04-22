4a125432d769d5000c4dc3f5675d12555a39f719
tramakarov <e2042912@gmail.com>
2020-04-22 18:27:14 +0500

    Update Picture size

--- src/components/Picture/Picture.jsx
+++ src/components/Picture/Picture.jsx
@@ -4,7 +4,7 @@ import ProgressiveImage from 'react-progressive-image'
 function Picture (props) {
   return (
       <ProgressiveImage src={props.quest.imageUrl} placeholder="/img/ww-quest-low.jpg">
-          {src => <img width = "1150" height = "300" src={src} className="img-fluid" alt="Responsive image" />}
+          {src => <img src={src} className="img-fluid" alt="Responsive image" />}
       </ProgressiveImage>
       )
 }
