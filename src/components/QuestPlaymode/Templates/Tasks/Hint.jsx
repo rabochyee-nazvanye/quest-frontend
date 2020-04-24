df757062259fe3b4ca3c789203f786efd2fbfd36
toplenboren <toplenboren@yandex.ru>
2020-04-25 01:38:48 +0500

    Fix bug when you couldn't access the /quests page, because of unhandled xption | lint

--- src/components/QuestPlaymode/Templates/Tasks/Hint.jsx
+++ src/components/QuestPlaymode/Templates/Tasks/Hint.jsx
@@ -6,11 +6,11 @@ import { PropTypes } from 'prop-types'
 export default function Hint (props) {
   return (
     <div className={'quest-task__container'}>
-        <div className={'quest-task__button'}>
-            <div className={'quest-task__typography'}>
-                <p>{props.title}</p>
-            </div>
+      <div className={'quest-task__button'}>
+        <div className={'quest-task__typography'}>
+          <p>{props.title}</p>
         </div>
+      </div>
     </div>
   )
 }
