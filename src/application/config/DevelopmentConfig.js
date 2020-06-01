2219659abb4857ce5520bd123f974e4b9f0fd07d
toplenboren <toplenboren@yandex.ru>
2020-06-01 16:30:14 +0500

    Fix a bug where we fetched the wrong dev address
    Signed-off-by: toplenboren <toplenboren@yandex.ru>

--- src/application/config/DevelopmentConfig.js
+++ src/application/config/DevelopmentConfig.js
@@ -1,7 +1,7 @@
 import AbstractConfig from './AbstractConfig'
 
 export default class DevelopmentConfig extends AbstractConfig{
-    BASE_URL = 'https://super-secret-dev.api.questspace.live'
+    BASE_URL = 'https://super-secret-dev-api.questspace.live'
     BACKEND_AUTH_PATH = '/session'
     BACKEND_AUTH_REGISTER_PATH = '/users'
     BACKEND_AUTH_FETCH_PATH = '/session'
