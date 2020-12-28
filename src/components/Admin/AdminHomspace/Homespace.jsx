44f1c98cf4a99b061d310ae4123b20ec24e03b4a
pornoiya <lactnovpidor@gmail.com>
2020-12-28 16:56:22 +0500

    created admin homespace

--- src/components/Admin/AdminHomspace/Homespace.jsx
+++ src/components/Admin/AdminHomspace/Homespace.jsx
@@ -1,43 +1,26 @@
 import React, {Component} from 'react'
 import {Tabs} from 'antd'
+import connect from 'react-redux/es/connect/connect'
 import './Homespace.css'
+import AdminBoard from './AdminStat'
 import QuestsList from "../../Account/AccountTemplate/QuestsList";
+import MetaTags from "../../shared/MetaTags/MetaTags";
+import {Api} from "../../../application/app";
 
 
 const {TabPane} = Tabs;
 
-const Demo = () => (
-    <div>
-        {/*вынесть статистику*/}
+function HomeSpace(props) {
+    return <div>
         <div className='primary-info'>
             <div className='primary-info__photo-element'>
-                <img src='https://sun1-88.userapi.com/impg/CvHkqmfghQxZ1yFqm-5GK6F5iGyBbniNoglDJQ/tdFX7t4_rgA.jpg?size=400x0&quality=90&crop=787,110,775,775&sign=ecc563012984e1ec9d33353c4ea6b89b&c_uniq_tag=T1-pQoJjxv6TgA8ZrAz8l7LV-SHyyJrz4EIeNSgrtL4&ava=1' alt='userpic'>
+                <img src={props.user_picture} alt='userpic'>
                 </img>
             </div>
             <div className='primary-info-name-location'>
-                <h2 className='user-name'>Егор Макаров</h2>
-                <h3 className='primary-info-geolocation'>Екатеринбург</h3>
+                <h2 className='user-name'>{props.username}</h2>
             </div>
-            <div className='admin-leader-board'>
-                <div className='admin-leader-board_done-quests'>
-                    <p className='admin-leader-board_done-quests__description'>
-                        Проведено квестов:
-                    </p>
-                    <h2>4</h2>
-                </div>
-                <div className='admin-leader-board_participated_quests'>
-                    <p className='admin-leader-board_participated_quests__description'>
-                        Участие в квестах:
-                    </p>
-                    <h2>4</h2>
-                </div>
-                <div className='admin-leader-board_total-tasks'>
-                    <p className='admin-leader-board_total-tasks__description'>
-                        Решено задач:
-                    </p>
-                    <h2>4%</h2>
-                </div>
-            </div>
+            <AdminBoard/>
         </div>
         <Tabs defaultActiveKey="1">
             <TabPane tab="Квесты" key="1">
@@ -47,13 +30,17 @@ const Demo = () => (
             </TabPane>
         </Tabs>
     </div>
-);
+}
 
 
 class Homespace extends Component {
-    render() {
-        return <Demo />;
+    render(props) {
+        return <HomeSpace username='Egor Makarov' user_picture='https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg'/>;
     }
 }
 
-export default Homespace
+const mapDispatchToProps = dispatch => ({
+    logout: () => { dispatch(Api.Auth.logout()) }
+})
+
+export default connect(null, mapDispatchToProps)(Homespace)
