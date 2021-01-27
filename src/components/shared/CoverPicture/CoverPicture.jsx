c5c7632bec37ac1a70174f34da3c97cc533e927a
toplenboren <toplenboren@yandex.ru>
2021-01-27 19:54:41 +0500

    Quest: Fix an image stretching for quest image

--- src/components/shared/CoverPicture/CoverPicture.jsx
+++ src/components/shared/CoverPicture/CoverPicture.jsx
@@ -5,7 +5,7 @@ import { PropTypes } from 'prop-types'
 
 export default function CoverPicture (props) {
   return (
-    <div className={'rounded-edges'}>
+    <div className={'rounded-edges '}>
       <ProgressiveImage src={props.url} placeholder="tiny-image.jpg">
         {src => <img src={src} className="cover-image__fluid cover-image__rounded-edges" alt="Обложка" />}
       </ProgressiveImage>
